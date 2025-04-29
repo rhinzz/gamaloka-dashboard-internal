779b1b4823313148203768c5670fbda11b09a39a
rhinzz <prama.sudjana@gmail.com>
2025-04-29 22:25:07 +0700

    Add sample donut chart

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Arrow Rotate
 const collapseElement = document.getElementById('settings');
 const arrow = collapseElement.closest('li').querySelector('.dropdown-arrow');
 
@@ -7,4 +8,31 @@ collapseElement.addEventListener('show.bs.collapse', () => {
 
 collapseElement.addEventListener('hide.bs.collapse', () => {
     arrow.classList.remove('rotate');
+});
+
+// Sample Donut Chart
+var yValues = [55, 49, 44, 24, 15];
+var barColors = [
+    "#b91d47",
+    "#00aba9",
+    "#2b5797",
+    "#e8c3b9",
+    "#1e7145"
+];
+
+new Chart("myChart", {
+    type: "doughnut",
+    data: {
+        labels: xValues,
+        datasets: [{
+            backgroundColor: barColors,
+            data: yValues
+        }]
+    },
+    options: {
+        title: {
+            display: true,
+            text: "World Wide Wine Production 2018"
+        }
+    }
 });
