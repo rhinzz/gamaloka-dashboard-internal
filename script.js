8b7000924852b6c127b99826ec2cc5c62622d7a6
rhinzz <prama.sudjana@gmail.com>
2025-05-02 16:12:38 +0700

    Modify donut chart

--- script.js
+++ script.js
@@ -10,16 +10,16 @@ collapseElement.addEventListener('hide.bs.collapse', () => {
     arrow.classList.remove('rotate');
 });
 
-
 // Sample Donut Chart
 var xValues = ["Jabodetabek", "Bandung", "Medan", "Jateng & DIY", "Jatim", "Bali"];
 var yValues = [34, 34, 34, 34, 34, 34];
 var barColors = [
-    "#b91d47",
-    "#00aba9",
-    "#2b5797",
-    "#e8c3b9",
-    "#1e7145"
+    "#FFD54F",
+    "#B36600",
+    "#FF8B13",
+    "#FFE5CC",
+    "#C62828",
+    "#EF9A9A"
 ];
 
 new Chart("firstChart", {
@@ -32,42 +32,10 @@ new Chart("firstChart", {
         }]
     },
     options: {
-        title: {
-            display: true,
-            text: "Subscriber"
-        },
         legend: {
-            position: "right"
-        }
-    },
-});
-
-var xValues = ["Jabodetabek", "Bandung", "Medan", "Jateng & DIY", "Jatim", "Bali"];
-var yValues = [34, 34, 34, 34, 34, 34];
-var barColors = [
-    "#b91d47",
-    "#00aba9",
-    "#2b5797",
-    "#e8c3b9",
-    "#1e7145"
-];
-
-new Chart("secondChart", {
-    type: "doughnut",
-    data: {
-        labels: xValues,
-        datasets: [{
-            backgroundColor: barColors,
-            data: yValues
-        }]
-    },
-    options: {
-        title: {
-            display: true,
-            text: "Unsubscriber"
+            display: false
         },
-        legend: {
-            position: "right"
-        }
+        responsive: false,
     }
 });
+
